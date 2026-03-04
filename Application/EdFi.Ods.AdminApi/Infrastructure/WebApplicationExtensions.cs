a623d1b480a1ca4bb34b63e7f6faa82d7ad35de3
Juan Agudelo <jagudelo@growthaccelerationpartners.com>
2026-03-04 08:34:45 -0500

    Remove unnecessary blank line in MapFeatureEndpoints method

--- Application/EdFi.Ods.AdminApi/Infrastructure/WebApplicationExtensions.cs
+++ Application/EdFi.Ods.AdminApi/Infrastructure/WebApplicationExtensions.cs
@@ -30,7 +30,6 @@ public static class WebApplicationExtensions
                     routeBuilder.MapEndpoints(application);
                 }
                 break;
-
             default:
                 throw new InvalidOperationException($"Invalid adminApiMode: {adminApiMode}");
         }
