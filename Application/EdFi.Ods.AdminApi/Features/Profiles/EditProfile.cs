c5af372977cfc8cc5e5b1ec52b72b3aa3cf57199
Jorge David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2026-08-28 10:48:40 -0600

    Undoing change on Application/EdFi.Ods.AdminApi/Features/Profiles/EditProfile.cs

--- Application/EdFi.Ods.AdminApi/Features/Profiles/EditProfile.cs
+++ Application/EdFi.Ods.AdminApi/Features/Profiles/EditProfile.cs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+    // SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -46,6 +46,7 @@ public class EditProfile : IFeature
         public string? Definition { get; set; }
 
         [SwaggerExclude]
+        [SwaggerSchema(Description = FeatureConstants.ProfileIdDescription, Nullable = false)]
         public int Id { get; set; }
     }
 
